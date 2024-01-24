2adbb55c144a30d84988ecb367676c14d9cbc2b3
Ankit.exe <ankitghimire321@gmail.com>
2024-01-24 22:10:55 +0545

    1st commit

---
+++ backend/src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import { Request, Response } from 'express';
+import mongoose from 'mongoose';
+
+
+mongoose.connect(process.env.MONGO_DB as string);
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("MONGO DB CONNECTION SUCCESSFULL");
+});
+
+connection.on("error", (err) => {
+  console.log("CONNECTION ERROR", err);
+});
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+ 
+
+
+app.listen(7000, () => {
+  console.log("PORT IS LISTING ON 7000");
+})
