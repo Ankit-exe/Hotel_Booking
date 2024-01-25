a38e481c333fd83e98bdd2596e7971ee347f63f0
Ankit.exe <ankitghimire321@gmail.com>
2024-01-25 13:45:39 +0545

    register api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
+import userRoutes from "./routes/users";
 
 
 mongoose.connect(process.env.MONGO_DB as string);
@@ -21,8 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
- 
-
+ app.use("/api/users",userRoutes)
 
 app.listen(7000, () => {
   console.log("PORT IS LISTING ON 7000");
