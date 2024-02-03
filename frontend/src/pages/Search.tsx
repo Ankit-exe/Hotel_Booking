6121e8ff7d2716cc6d8c1929a0ab974488c72545
Ankit.exe <ankitghimire321@gmail.com>
2024-02-03 14:54:48 +0545

    pagination

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -3,6 +3,7 @@ import { useSearchContext } from "../context/SearchContext";
 import * as apiClient from "../api-client";
 import { useState } from "react";
 import SearchResultsCard from "../components/SearchResultCard";
+import Pagination from "../components/Pagination";
 
 export const Search = () => {
   const search = useSearchContext();
@@ -17,28 +18,36 @@ export const Search = () => {
     page: page.toString(),
   };
 
-  const { data: hotelData } = useQuery(["searchHotels", searchParams], () => apiClient.searchHotels(searchParams));
+  const { data: hotelData } = useQuery(["searchHotels", searchParams], () =>
+    apiClient.searchHotels(searchParams)
+  );
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
-        <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
-            <div className="space-y-5">
-                <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
-                    Filter By:
-                </h3>
-
-            </div>
+      <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
+        <div className="space-y-5">
+          <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
+            Filter By:
+          </h3>
         </div>
-        <div className="flex flex-col gap-5">
-            <div className="flex justify-between items-center">
-                <span className="text-xl font-bold">
-                    {hotelData?.pagination.total} Hotels found
-                    {search.destination ? ` in ${search.destination}` : ""}
-                </span>
-            </div>
-            {hotelData?.data.map((hotel)=>(
-                <SearchResultsCard hotel={hotel}/>
-            ))}
+      </div>
+      <div className="flex flex-col gap-5">
+        <div className="flex justify-between items-center">
+          <span className="text-xl font-bold">
+            {hotelData?.pagination.total} Hotels found
+            {search.destination ? ` in ${search.destination}` : ""}
+          </span>
         </div>
+        {hotelData?.data.map((hotel) => (
+          <SearchResultsCard hotel={hotel} />
+        ))}
+        <div>
+          <Pagination
+            page={hotelData?.pagination.page || 1}
+            pages={hotelData?.pagination.pages || 1}
+            onPageChange={(page) => {setPage(page)}}
+          />
+        </div>
+      </div>
     </div>
   );
 };
